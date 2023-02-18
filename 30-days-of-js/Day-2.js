de58ea77b613781aec4d3b36ed81eae48bee5f3a
unknown <williambenet@hotmail.com>
2023-02-18 10:54:50 -0300

    first

---
+++ 30-days-of-js/Day-2.js
@@ -0,0 +1,22 @@
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days of Javascript'
+
+// 2. Print the string on the browser console using console.log()
+console.log(challenge)
+
+// 3. Print the length of the string on the browser console using console.log()
+console.log(challenge.length)
+
+// 4. Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase())
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase())
+
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
+console.log(challenge.substring(0,1))
+
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log(challenge.substring(3))
+
+// 8. Check if the string contains a word Script using includes() method
